f599544b3796d763848bbc4d1e164c0cfbad8a68
ThanhDoanCoder <nguyendoanthanh4444@gmail.com>
2022-05-11 03:42:00 +0700

    add ShowInfor function in Student class

--- Nguyen-Doan-Thanh/VerssionControl/VerssionControl/Student.cs
+++ Nguyen-Doan-Thanh/VerssionControl/VerssionControl/Student.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace VerssionControl
 {
-    internal class Student
+    public class Student
     {
+        private string _Name;
+        private string _Email;
+        private int _Age;
+        public Student(string name, string email, int age)
+        {
+            _Name = name;
+            _Email = email; 
+            _Age = age;
+        }
+        public void ShowInfor()
+        {
+            Console.WriteLine($"name : {_Name}, age: {_Age}");
+        }
+
     }
 }
