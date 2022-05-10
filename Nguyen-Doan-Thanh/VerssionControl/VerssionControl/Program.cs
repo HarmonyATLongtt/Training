ec4116c94946532bde07d2e494cc2c4df54778d2
ThanhDoanCoder <nguyendoanthanh4444@gmail.com>
2022-05-11 03:29:55 +0700

    update class Program

--- Nguyen-Doan-Thanh/VerssionControl/VerssionControl/Program.cs
+++ Nguyen-Doan-Thanh/VerssionControl/VerssionControl/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace VerssionControl
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello world");
         }
     }
 }
