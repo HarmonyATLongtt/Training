a6d2c771375fb7c4712dcc2fa6524bd1b76c43d1
Thanh_NewbieDev <105188286+ThanhHaT@users.noreply.github.com>
2022-05-17 09:35:30 +0700

    first commit (#3)
    * first commit
    
    * update class Program
    
    * add Student class
    
    * add ShowInfor function in Student class
    
    * delete Student class and add Teacher class
    
    * restore Student class from former commit
    
    * Revert " restore Student class from former commit"
    
    This reverts commit bac61b9a98a5fb9010dffffe81d948c0be13f3e1.
    
    * ignore files
    
    * update Teacher class
    
    * commit to resolve conflict
    
    Co-authored-by: HarmonyATLong <longtt@harmony-at.vn>

---
+++ Nguyen-Doan-Thanh/VerssionControl/VerssionControl/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerssionControl
+{
+    public class Teacher
+    {
+        private string _Name;
+        private string _Description;
+        private int _Age;
+        public Teacher(string name, int age)
+        {
+            _Name = name;
+            _Age = age;
+        }
+        public void ShowInfor()
+        {
+            Console.WriteLine(_Description);
+            // comment for conflict resolving 
+            Console.WriteLine(_Name);
+        }
+    }
+}
