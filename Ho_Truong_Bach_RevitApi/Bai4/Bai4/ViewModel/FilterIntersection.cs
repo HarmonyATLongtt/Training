3bcc31e9befaa881bf851a7819b172a4158f43cc
hotruongbach <85720530+hotruongbach@users.noreply.github.com>
2022-08-02 18:01:08 +0700

    upload code (#9)
    * upload code
    
    * Upload Code
    
    * delete folder
    
    * upload code
    
    * Upload code
    
    * code
    
    * upload code
    
    * test conflict
    
    Co-authored-by: HarmonyATLong <longtt@harmony-at.vn>

---
+++ Ho_Truong_Bach_RevitApi/Bai4/Bai4/ViewModel/FilterIntersection.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4.ViewModel
+{
+    public class FilterIntersection : IEqualityComparer<XYZ>
+    {
+        /// <summary>
+        /// compare two objects with the same position X or Y
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool Equals(XYZ x, XYZ y)
+        {
+            if (Object.ReferenceEquals(x, y)) return true;
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+            return x.IsAlmostEqualTo(y);
+        }
+        /// <summary>
+        /// get hash code of the compared object
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public int GetHashCode(XYZ obj)
+        {
+            return 1;
+        }
+    }
+}
