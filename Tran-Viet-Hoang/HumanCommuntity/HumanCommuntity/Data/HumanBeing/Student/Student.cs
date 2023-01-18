90aa2bb80b823e30ef3228be615f03fcbb4efb1b
Trần Việt Hoàng <hoang365389@gmail.com>
2023-01-18 09:19:58 +0700

    refactor HumanCommunity

---
+++ Tran-Viet-Hoang/HumanCommuntity/HumanCommuntity/Data/HumanBeing/Student/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HumanCommuntity.Data.HumanBeing.Student
+{
+    public class Student : HumanBeing
+    {
+        public string Class { get; set; }
+        public new void Communicate()
+        {
+
+            Console.WriteLine("Students are so noisy");
+        }
+        public void Job() { Console.WriteLine("Do homework"); }
+    }
+}
--- Tran-Viet-Hoang/HumanCommuntity/HumanCommuntity/Program.cs
+++ Tran-Viet-Hoang/HumanCommuntity/HumanCommuntity/Program.cs
@@ -1,78 +1,34 @@
-﻿using System;
+﻿using HumanCommuntity.Data.HumanBeing;
+using HumanCommuntity.Data.HumanBeing.Doctor;
+using HumanCommuntity.Data.HumanBeing.Student;
+using HumanCommuntity.Data.HumanBeing.Teacher;
+using System;
 
 namespace HumanCommuntity
 {
-
-
-    public class HumanBeing 
+    public class Program
     {
-        public string Name { get; set; }
-        public double Age { get; set; }
-        public double Height { get; set; }
-        public string Sex { get; set; }
-        public virtual void Communicate () { Console.WriteLine("Talk"); }
-        public virtual void Eating () { Console.WriteLine("Eat"); }
-
-        public virtual void Sleeping () { Console.WriteLine("Sleep"); }
-
-        public virtual void Job() { Console.WriteLine("Do things");}
-    }
-
-    public class Student : HumanBeing 
-    {
-        public string Class { get; set; }
-        public override void Communicate()
+        private static void Main(string[] args)
         {
+            HumanBeing hocsinh = new Student { Name = "cháu Bo tiêu hoc", Age = 10, Sex = "Nam" };
+            Student hocsinh1 = new Student { Name = "cháu Bo tiêu hoc", Age = 10, Sex = "Nam" };
+            HumanBeing bacsi = new Doctor { Name = "bác sĩ Đông", Age = 38, Sex = "Nam" };
+            HumanBeing giaovien = new Teacher { Name = "cô giáo Kim", Age = 32, Sex = "Nư" };
 
-            Console.WriteLine("Students are so noisy");
-        }
-        public void Job() { Console.WriteLine("Do homework"); }
-    }
+            Console.WriteLine(hocsinh.Name + " " + hocsinh.Age + " tuôi, giơi tính: " + hocsinh.Sex);
+            hocsinh.Communicate(); //not override: Talk
+            hocsinh1.Communicate(); // hiding....: Students are so noisy
 
-    public class Teacher : HumanBeing 
-    { 
-        public string Subject { get; set; }
-        public override void Communicate()
-        {
-
-            Console.WriteLine("Teacher required all of student to keep quite");
-        }
-        public void Job() { Console.WriteLine("Do teaching"); }
-    }
-
-    public class Doctor : HumanBeing
-    {
-        public string Major { get; set; }
-        public override void Communicate()
-        {
-
-            Console.WriteLine("Doctor explains how to cure that kind of illness");
-        }
-        public void Job() { Console.WriteLine("Do healthchecking"); }
-    }
-
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-
-            HumanBeing hocsinh = new Student { Name = "cháu Bo tiểu học" , Age = 10 , Sex = "Nam" };
-            HumanBeing bacsi = new Doctor { Name = "bác sĩ Đông" , Age = 38, Sex = "Nam" };
-            HumanBeing giaovien = new Teacher { Name = "cô giáo Kim", Age = 32, Sex = "Nữ"  };
-
-            Console.WriteLine(hocsinh.Name + " " + hocsinh.Age + " tủi, giới tính: " + hocsinh.Sex);
-            hocsinh.Communicate(); //override
             hocsinh.Job();         //not override
             Console.Write("\n");
 
-            Console.WriteLine(bacsi.Name + " " + bacsi.Age + " tủi, giới tính: " + bacsi.Sex);
-            bacsi.Communicate();
+            Console.WriteLine(bacsi.Name + " " + bacsi.Age + " tuôi, giơi tính: " + bacsi.Sex);
+            bacsi.Communicate(); //override
             bacsi.Job();
-            Environment.NewLine.Clone();
             Console.Write("\n");
 
-            Console.WriteLine(giaovien.Name + " " + giaovien.Age + " tủi, giới tính: " + giaovien.Sex);
-            giaovien.Communicate();
+            Console.WriteLine(giaovien.Name + " " + giaovien.Age + " tuôi, giơi tính: " + giaovien.Sex);
+            giaovien.Communicate(); //override
             giaovien.Job();
 
             Console.ReadLine();
