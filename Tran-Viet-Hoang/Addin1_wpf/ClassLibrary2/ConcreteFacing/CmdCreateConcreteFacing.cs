de71d2bff59533af3a81d0b032e445c0286856ac
Trần Việt Hoàng <hoang365389@gmail.com>
2023-02-14 10:11:01 +0700

    CoverApplying (1)
    - Selection: Just highlight beam or column even when user selection have other kind of elem
    - Apply cover: can apply cover to all the face that elem has, covers fit to elem shape even when elem is rotated randomly

---
+++ Tran-Viet-Hoang/Addin1_wpf/ClassLibrary2/ConcreteFacing/CmdCreateConcreteFacing.cs
@@ -0,0 +1,148 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using ConcreteFacing.UI.ViewModel;
+using ConcreteFacing.UI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace ConcreteFacing
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class CmdCreateConcreteFacing : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            MainView view = new MainView();
+            view.DataContext = new MainViewModel();
+
+            var elems = PickConcreteBeamOrColumn(uidoc);
+            //var elems = SelectedConcreteBeamOrColumn(uidoc);
+            int num = elems.Count();
+
+            if (elems != null && view.ShowDialog() == false)
+            {
+                using (var transaction = new Transaction(doc, "Set Mark"))
+                {
+                    transaction.Start();
+                    foreach (var elem in elems)
+                    {
+                        FamilyInstance ins = elem as FamilyInstance;
+                        BoundingBoxXYZ boundingbox = ins.get_BoundingBox(null);
+                        string info = "Cấu kiện " + ins.Name + " có:" + "\n" +
+                           "Vị trí Min BoundingBox là: " + boundingbox.Min.ToString() + "\n" +
+                           "Vị trí Max BoundingBox là: " + boundingbox.Max.ToString();
+
+                        double thickness = 20 / 304.8;
+
+                        var Sol = elem.get_Geometry(new Options())
+                            .OfType<Solid>()
+                            .ToList();
+                        if (Sol.Count() != 0)
+                        {
+                            CreateDirectShape(doc, Sol, thickness);
+                        }
+                        else
+                        {
+                            var InsGeometry = elem.get_Geometry(new Options())
+                            .OfType<GeometryInstance>()
+                            .SelectMany(x => x.GetInstanceGeometry().OfType<Solid>())
+                            .ToList()
+                            ;
+                            CreateDirectShape(doc, InsGeometry, thickness);
+                        }
+                    }
+                    MessageBox.Show("Đã apply covers cho " + num + " cấu kiện");
+                    transaction.Commit();
+                }
+            }
+            else
+                MessageBox.Show("nguoi dung huy thao tac");
+
+            return Result.Succeeded;
+        }
+
+        private List<Element> PickConcreteBeamOrColumn(UIDocument uidoc)
+        {
+            try
+            {
+                List<Element> result = new List<Element>();
+                IList<Reference> refer = uidoc.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element, new Common.AllowBeamAndColumn(), "chon dam hoac cot be tong") as IList<Reference>;
+                if (refer != null)
+
+                    foreach (Reference r in refer)
+                    {
+                        result.Add(uidoc.Document.GetElement(r));
+                    }
+                return result;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            return null;
+        }
+
+        private List<Element> SelectedConcreteBeamOrColumn(UIDocument uidoc)
+        {
+            try
+            {
+                ICollection<ElementId> result = new List<ElementId>();
+                List<Element> allelem = uidoc.Selection.GetElementIds().Select(id => uidoc.Document.GetElement(id)).ToList();
+                List<Element> beamandcol = new List<Element>();
+                foreach (Element ele in allelem)
+                {
+                    FamilyInstance ins = ele as FamilyInstance;
+                    if (ins != null && (ins.StructuralType == Autodesk.Revit.DB.Structure.StructuralType.Column ||
+                        ins.StructuralType == Autodesk.Revit.DB.Structure.StructuralType.Beam))
+                    {
+                        beamandcol.Add(ele);
+                        result.Add(ele.Id);
+                    }
+                }
+
+                return beamandcol;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            return null;
+        }
+
+        public void CreateDirectShape(Document doc, List<Solid> solids, double thickness)
+        {
+            string facenumber = null;
+            string facenormal = null;
+            List<Face> facelist = new List<Face>();
+            foreach (var solid in solids)
+            {
+                var faces = solid.Faces.Size;
+                facenumber = faces.ToString();
+                if (solid.Volume != 0)
+                {
+                    foreach (Face face in solid.Faces)
+                    {
+                        facenormal += (face as PlanarFace).FaceNormal.ToString() + "\n";
+                        facelist.Add(face);
+
+                        var eloop = face.GetEdgesAsCurveLoops();
+                        Solid cover = GeometryCreationUtilities.CreateExtrusionGeometry(eloop, (face as PlanarFace).FaceNormal, thickness);
+                        DirectShape ds = DirectShape.CreateElement(doc, new ElementId(BuiltInCategory.OST_GenericModel));
+                        ds.SetShape(new GeometryObject[] { cover });
+                    }
+                }
+            }
+
+            //int num = solids.Count();
+            //MessageBox.Show("Số solid mà ins có: " + num.ToString() + "\n" +
+            //    "Số face mà ins có: " + facelist.Count() + "\n" +
+            //    "Pháp tuyển của các face là: " + "\n" + facenormal);
+        }
+    }
+}
---
+++ Tran-Viet-Hoang/Addin1_wpf/ClassLibrary2/ConcreteFacing/UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Input;
+
+namespace ConcreteFacing.UI.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        //tao itemsource de bind vao listbox
+        private ObservableCollection<DataTable> _tables;
+
+        public ObservableCollection<DataTable> Tables
+        {
+            get => _tables;
+            set
+            {
+                _tables = value;
+                RaisePropertyChange(nameof(Tables));
+            }
+        }
+
+        public ICommand CreateCommand { get; set; }
+
+        public MainViewModel()
+        {
+            CreateCommand = new RelayCommand(CreateCommandInvoke);
+        }
+
+        private void CreateCommandInvoke()
+        {
+            DataTable Beam = new DataTable();
+            DataTable Column = new DataTable();
+
+            Beam.TableName = "Dầm";
+            Column.TableName = "Cột";
+
+            List<DataTable> list = new List<DataTable>() { Beam, Column };
+
+            Tables = new ObservableCollection<DataTable>(list);
+        }
+        private void CancelCommandInvoke(object parameter)
+        {
+            if (parameter is System.Windows.Window wnd)
+            {
+                wnd.DialogResult = false;
+                wnd.Close();
+            }
+        }
+
+        public void RaisePropertyChange(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
