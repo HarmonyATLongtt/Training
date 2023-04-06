915a40eb81b3b01cbe03d4beb76bdbc2cff4b265
TruongGiang <giangcuathoidai@gmail.com>
2023-04-06 09:13:57 +0700

    Trainees/Nguyen-Truong-Giang_Merging-code
    Vi du ve Struct

---
+++ Vi-Du-Struct/Vi-Du-Struct/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vi_Du_Struct
+{
+    class Program
+    {
+        struct SinhVien
+        {
+            public int MaSV;
+            public string HoTen;
+            public double DiemToan;
+            public double DiemLy;
+        }
+        static void NhapThongTinSinhVien(out SinhVien SV)
+        {
+            Console.Write(" Ma so: ");
+            SV.MaSV = int.Parse(Console.ReadLine());
+            Console.Write(" Ho ten: ");
+            SV.HoTen = Console.ReadLine();
+            Console.Write(" Diem toan: ");
+            SV.DiemToan = Double.Parse(Console.ReadLine());
+            Console.Write(" Diem ly: ");
+            SV.DiemLy = Double.Parse(Console.ReadLine());
+        }
+
+        static void XuatThongTinSinhVien(SinhVien SV)
+        {
+            Console.WriteLine(" Ma so: " + SV.MaSV);
+            Console.WriteLine(" Ho ten: " + SV.HoTen);
+            Console.WriteLine(" Diem toan: " + SV.DiemToan);
+            Console.WriteLine(" Diem ly: " + SV.DiemLy);
+        }
+
+        static void Main(string[] args)
+        {
+            SinhVien SV1 = new SinhVien();
+            Console.WriteLine(" Nhap thong tin sinh vien: ");
+            NhapThongTinSinhVien(out SV1);
+
+
+            Console.WriteLine("*********");
+
+            Console.WriteLine(" Thong tin sinh vien vua nhap la: ");
+            XuatThongTinSinhVien(SV1);
+
+            Console.ReadLine();
+        }
+    }
+}
