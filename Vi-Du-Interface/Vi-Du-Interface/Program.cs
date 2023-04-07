df629ae5c7729538eb866e114159316658fe09eb
TruongGiang <giangcuathoidai@gmail.com>
2023-04-07 11:06:32 +0700

    Vi-Du-Interface
    Vi-Du-Interface

---
+++ Vi-Du-Interface/Vi-Du-Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vi_Du_Interface
+{
+    interface Ispeak
+    {
+        void speak();
+    }
+
+    class Animal : Ispeak
+    {
+        public void speak()
+        {
+            Console.WriteLine("Animal is speaking. . .");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            animal.speak();
+
+            Console.ReadKey();
+        }
+    }
+}
