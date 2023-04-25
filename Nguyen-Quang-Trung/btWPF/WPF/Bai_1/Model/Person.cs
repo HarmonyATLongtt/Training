b57a841c8a2d61a00a0d580caaa7a5cb43bbb8bf
nqtrung2109 <trung21092000@gmail.com>
2023-04-25 11:14:07 +0700

    update bai 1 bt WPF

---
+++ Nguyen-Quang-Trung/btWPF/WPF/Bai_1/Model/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1.Model
+{
+    public class Person
+    {
+        public string ID { get; set; }
+        public string Ten { get; set; }
+        public string Tuoi { get; set; }
+        public string DiaChi { get; set; }
+        public string ThuNhap { get; set; }
+        public string HeSoThue { get; set; }
+
+        //public Person(string id, string ten, string tuoi, string diachi, string thuNhap, string heSoThue)
+        //{
+        //    ID = id;
+        //    Ten = ten;
+        //    Tuoi = tuoi;
+        //    DiaChi = diachi;
+        //    ThuNhap = thuNhap;
+        //    HeSoThue = heSoThue;
+        //}
+    }
+}
---
+++ Nguyen-Quang-Trung/testC#/TimHieuWPF/TreeView_WPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeView_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MenuItem root = new MenuItem { Title = "Menu" };
+            MenuItem child1 = new MenuItem { Title = "item 1" };
+            child1.Items.Add(new MenuItem { Title = "child item 1.1" });
+            child1.Items.Add(new MenuItem { Title = "child item 1.2" });
+            MenuItem child2 = new MenuItem { Title = "item 2" }; ;
+            root.Items.Add(child1);
+            root.Items.Add(child2);
+            trvMenu.Items.Add(root);
+        }
+    }
+
+    public class MenuItem
+    {
+        public MenuItem()
+        {
+            this.Items = new ObservableCollection<MenuItem>();
+        }
+
+        public string Title { get; set; }
+
+        public ObservableCollection<MenuItem> Items { get; set; }
+    }
+}
