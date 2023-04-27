11671adbba19db153ba9bd50046c2fcb9b4d4b31
nqtrung2109 <trung21092000@gmail.com>
2023-04-27 20:17:52 +0700

    update bai 2
    sua loi crash voi file sample.xlsx và control resize theo main window

--- Nguyen-Quang-Trung/btWPF/WPF/Bai_2/ViewModel/MainViewModel.cs
+++ Nguyen-Quang-Trung/btWPF/WPF/Bai_2/ViewModel/MainViewModel.cs
@@ -2,6 +2,7 @@
 using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.IO;
@@ -28,9 +29,9 @@ namespace Bai_2.ViewModel
             }
         }
 
-        public List<string> _sheetName;
+        public ObservableCollection<string> _sheetName;
 
-        public List<string> SheetName
+        public ObservableCollection<string> SheetName
         {
             get { return _sheetName; }
             set
@@ -40,30 +41,31 @@ namespace Bai_2.ViewModel
             }
         }
 
-        private List<DataTable> _listDataTable = new List<DataTable>();
-        public List<string> _listSheet = new List<string>();
+        private ObservableCollection<DataTable> _listDataTable = new ObservableCollection<DataTable>();
+        public ObservableCollection<string> _listSheet = new ObservableCollection<string>();
 
-        //private DataTable _selectedSheetData;
+        //private string _selectedSheet;
 
-        //public DataTable SelectedSheetData
+        //public string SelectedSheet
         //{
-        //    get { return _selectedSheetData; }
+        //    get { return _selectedSheet; }
         //    set
         //    {
-        //        _selectedSheetData = value;
-        //        OnPropertyChanged(nameof(SelectedSheetData));
+        //        _selectedSheet = value;
+        //        int index = SheetName.IndexOf(value);
+        //        CurrentSheet = _listDataTable[index];
         //    }
         //}
 
         public ICommand ImportFileCommand { get; set; }
         public ICommand ExportFileCommand { get; set; }
-        public ICommand SelectSheetCommand { get; set; }
+        //public ICommand SelectSheetCommand { get; set; }
 
         public MainViewModel()
         {
             ImportFileCommand = new RelayCommand<object>(ImportFile);
             ExportFileCommand = new RelayCommand<object>(ExportFile);
-            SelectSheetCommand = new RelayCommand<object>(SelectSheet);
+            //SelectSheetCommand = new RelayCommand<object>(SelectSheet);
         }
 
         public void ExportFile(object obj)
@@ -94,23 +96,21 @@ namespace Bai_2.ViewModel
             package.Save();
         }
 
-        public void SelectSheet(object obj)
-        {
-            if (SheetName == null || SheetName.Count == 0)
-            {
-                return;
-            }
-            string selectedSheet = obj as string;
-            int index = SheetName.IndexOf(selectedSheet);
-            CurrentSheet = _listDataTable[index];
-            OnPropertyChanged(nameof(CurrentSheet));
-        }
+        //public void SelectSheet(object obj)
+        //{
+        //    if (SheetName == null || SheetName.Count == 0)
+        //    {
+        //        return;
+        //    }
+        //    string selectedSheet = obj as string;
+        //    int index = SheetName.IndexOf(selectedSheet);
+        //    CurrentSheet = _listDataTable[index];
+        //}
 
         public void ImportFile(object obj)
         {
             string filePath = "";
             //var listDataTable = new List<DataTable>();
-
             //var listSheet = new List<string>();
 
             var importFileDialog = new OpenFileDialog();
@@ -127,22 +127,48 @@ namespace Bai_2.ViewModel
             }
             var package = new ExcelPackage(new FileInfo(filePath));
             ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
-            int i = 1;
+            int x = 1;
             foreach (var workSheet in package.Workbook.Worksheets)
             {
                 var dataTable = new DataTable();
-                dataTable.TableName = $"Table ({i})";
-                dataTable = workSheet.Cells[1, 1, workSheet.Dimension.End.Row, workSheet.Dimension.End.Column].ToDataTable(c =>
+                // Thêm tên cột vào DataTable
+                for (int i = 1; i <= workSheet.Dimension.End.Column; i++)
                 {
-                    c.FirstRowIsColumnNames = true;
-                });
+                    dataTable.Columns.Add(workSheet.Cells[1, i].Value.ToString());
+                }
+                // Duyệt từng cell trong tệp Excel và thêm vào DataTable
+                for (int row = 2; row <= workSheet.Dimension.End.Row; row++)
+                {
+                    DataRow dr = dataTable.NewRow();
+
+                    for (int col = 1; col <= workSheet.Dimension.End.Column; col++)
+                    {
+                        if (workSheet.Cells[row, col].Value != null)
+                        {
+                            if (workSheet.Cells[row, col].Value.GetType() == typeof(DateTime))
+                            {
+                                dr[col - 1] = workSheet.Cells[row, col].GetValue<DateTime>();
+                            }
+                            else
+                            {
+                                dr[col - 1] = workSheet.Cells[row, col].Value.ToString();
+                            }
+                        }
+                        else
+                        {
+                            dr[col - 1] = DBNull.Value;
+                        }
+                    }
+
+                    dataTable.Rows.Add(dr);
+                }
+
                 string newTableName = workSheet.Name;
                 while (_listDataTable.Any(dt => dt.TableName == newTableName))
                 {
-                    newTableName = $"{workSheet.Name} ({i})";
-                    i++;
+                    newTableName = $"{workSheet.Name} ({x})";
+                    x++;
                 }
-
                 // Gán tên mới cho bảng
                 dataTable.TableName = newTableName;
 
@@ -150,7 +176,7 @@ namespace Bai_2.ViewModel
                 _listSheet.Add(workSheet.Name);
             }
             SheetName = _listSheet;
-            CurrentSheet = _listDataTable[0];
+            CurrentSheet = _listDataTable[2];
         }
     }
 }
