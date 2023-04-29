c257ebab4f131054da064186216b9325493d54bb
TruongGiang <giangcuathoidai@gmail.com>
2023-04-29 21:38:21 +0700

    Commit fix Export File

--- Nguyen_Truong_Giang/Project-WPF/Ex WPF/Ex WPF/ModelView/BaseViewModel.cs
+++ Nguyen_Truong_Giang/Project-WPF/Ex WPF/Ex WPF/ModelView/BaseViewModel.cs
@@ -63,9 +63,6 @@ namespace Ex_WPF.ModelView
         public ICommand ImportFileCommand { get; set; }
         public ICommand ExportFileCommand { get; set; }
         public ICommand ClearCommand { get; set; }
-        public ICommand SelectionSheet1Command { get; set; }
-        public ICommand SelectionSheet2Command { get; set; }
-        public ICommand SelectionSheet3Command { get; set; }
         public ICommand NextSheetCommand { get; set; }
         public ICommand BackSheetCommand { get; set; }
 
@@ -73,9 +70,6 @@ namespace Ex_WPF.ModelView
         {
             ImportFileCommand = new RelayCommand<object>(ImportFile);
             ExportFileCommand = new RelayCommand<object>(ExportFile);
-            SelectionSheet1Command = new RelayCommand<object>(SelectionSheet1);
-            SelectionSheet2Command = new RelayCommand<object>(SelectionSheet2);
-            SelectionSheet3Command = new RelayCommand<object>(SelectionSheet3);
             ClearCommand = new RelayCommand<object>(Clear);
             NextSheetCommand = new RelayCommand<object>(NextSheet);
             BackSheetCommand = new RelayCommand<object>(BackSheet);
@@ -199,67 +193,23 @@ namespace Ex_WPF.ModelView
             {
                 using (ExcelPackage package = new ExcelPackage())
                 {
-                    if (Selection == _student)
+                    ExcelWorksheet newSheet = package.Workbook.Worksheets.Add("Sheet1");
+                    newSheet.Cells[1, 1].Value = "ID";
+                    newSheet.Cells[1, 2].Value = "Name";
+                    newSheet.Cells[1, 3].Value = "Age";
+                    newSheet.Cells[1, 4].Value = "Address";
+                    newSheet.Cells[1, 5].Value = "TaxFactor";
+
+                    int rowIndex = 2;
+                    foreach (Person person in Selection)
                     {
-                        ExcelWorksheet studentSheet = package.Workbook.Worksheets.Add("Student");
-                        studentSheet.Cells[1, 1].Value = "ID";
-                        studentSheet.Cells[1, 2].Value = "Name";
-                        studentSheet.Cells[1, 3].Value = "Age";
-                        studentSheet.Cells[1, 4].Value = "Address";
-                        studentSheet.Cells[1, 5].Value = "TaxFactor";
-
-                        int rowIndex = 2;
-                        foreach (Person student in Selection)
-                        {
-                            studentSheet.Cells[rowIndex, 1].Value = student.ID;
-                            studentSheet.Cells[rowIndex, 2].Value = student.Name;
-                            studentSheet.Cells[rowIndex, 3].Value = student.Age;
-                            studentSheet.Cells[rowIndex, 4].Value = student.Address;
-                            studentSheet.Cells[rowIndex, 5].Value = student.TaxFactor;
-                            rowIndex++;
-                        }
+                        newSheet.Cells[rowIndex, 1].Value = person.ID;
+                        newSheet.Cells[rowIndex, 2].Value = person.Name;
+                        newSheet.Cells[rowIndex, 3].Value = person.Age;
+                        newSheet.Cells[rowIndex, 4].Value = person.Address;
+                        newSheet.Cells[rowIndex, 5].Value = person.TaxFactor;
+                        rowIndex++;
                     }
-                    else if (Selection == _teacher)
-                    {
-                        ExcelWorksheet teacherSheet = package.Workbook.Worksheets.Add("Teacher");
-                        teacherSheet.Cells[1, 1].Value = "ID";
-                        teacherSheet.Cells[1, 2].Value = "Name";
-                        teacherSheet.Cells[1, 3].Value = "Age";
-                        teacherSheet.Cells[1, 4].Value = "Address";
-                        teacherSheet.Cells[1, 5].Value = "TaxFactor";
-
-                        int rowIndex = 2;
-                        foreach (Person teacher in Selection)
-                        {
-                            teacherSheet.Cells[rowIndex, 1].Value = teacher.ID;
-                            teacherSheet.Cells[rowIndex, 2].Value = teacher.Name;
-                            teacherSheet.Cells[rowIndex, 3].Value = teacher.Age;
-                            teacherSheet.Cells[rowIndex, 4].Value = teacher.Address;
-                            teacherSheet.Cells[rowIndex, 5].Value = teacher.TaxFactor;
-                            rowIndex++;
-                        }
-                    }
-                    else if (Selection == _employee)
-                    {
-                        ExcelWorksheet employeeSheet = package.Workbook.Worksheets.Add("Employees");
-                        employeeSheet.Cells[1, 1].Value = "ID";
-                        employeeSheet.Cells[1, 2].Value = "Name";
-                        employeeSheet.Cells[1, 3].Value = "Age";
-                        employeeSheet.Cells[1, 4].Value = "Address";
-                        employeeSheet.Cells[1, 5].Value = "TaxFactor";
-
-                        int rowIndex = 2;
-                        foreach (Person employees in Selection)
-                        {
-                            employeeSheet.Cells[rowIndex, 1].Value = employees.ID;
-                            employeeSheet.Cells[rowIndex, 2].Value = employees.Name;
-                            employeeSheet.Cells[rowIndex, 3].Value = employees.Age;
-                            employeeSheet.Cells[rowIndex, 4].Value = employees.Address;
-                            employeeSheet.Cells[rowIndex, 5].Value = employees.TaxFactor;
-                            rowIndex++;
-                        }
-                    }
-
                     package.SaveAs(new FileInfo(saveFileDialog.FileName));
                 }
 
@@ -272,26 +222,12 @@ namespace Ex_WPF.ModelView
             }
         }
 
-        public void SelectionSheet1(object obj)
-        {
-            Selection = _student;
-        }
-
-        public void SelectionSheet2(object obj)
-        {
-            Selection = _teacher;
-        }
-
-        public void SelectionSheet3(object obj)
-        {
-            Selection = _employee;
-        }
-
         public void Clear(object obj)
         {
             _student.Clear();
             _teacher.Clear();
             _employee.Clear();
+            _selection.Clear();
             MessageBox.Show("Clear successful!");
         }
 
@@ -302,7 +238,7 @@ namespace Ex_WPF.ModelView
             {
                 InitSheet(index);
             }
-            if(index >= sheets.Count)
+            if (index >= sheets.Count)
             {
                 index = 0;
                 InitSheet(index);
@@ -314,7 +250,7 @@ namespace Ex_WPF.ModelView
         public void BackSheet(object obj)
         {
             index--;
-            if(index >= 0)
+            if (index >= 0)
             {
                 InitSheet(index);
             }
