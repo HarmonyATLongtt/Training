deaabc79712ebfd6b27aed17672c1003a5693501
TruongGiang <giangcuathoidai@gmail.com>
2023-05-16 17:24:08 +0700

    Commit 16/5 exRevit

---
+++ Nguyen_Truong_Giang/Project-Revit/ExRevitAPI/ExRevitAPI/FilterInstance.cs
@@ -0,0 +1,66 @@
+﻿#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+#endregion
+
+namespace ExRevitAPI
+{
+    [Transaction(TransactionMode.Manual)]
+    public class FilterInstance : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message,ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // Access current selection
+
+            Selection sel = uidoc.Selection;
+
+            // create collector
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            ICollection<Element> allElements = collector.WhereElementIsNotElementType().ToElements();
+
+            // Đếm số lượng category
+            HashSet<string> categories = new HashSet<string>();
+            foreach (Element element in allElements)
+            {
+                categories.Add(element.Name);
+            }
+            int categoryCount = categories.Count;
+
+            // Đếm số lượng family
+            HashSet<string> families = new HashSet<string>();
+            foreach (Element element in allElements)
+            {
+                families.Add(element.Name);
+            }
+            int familyCount = families.Count;
+
+            // Đếm số lượng type
+            int typeCount = allElements.Select(element => element.GetTypeId()).Distinct().Count();
+
+            // Đếm số lượng instance
+            int instanceCount = allElements.Count;
+
+            // In ra thông tin
+            TaskDialog.Show("Instance Information",
+                "Số lượng Category: " + categoryCount.ToString() + "\n" +
+                "Số lượng Family: " + familyCount.ToString() + "\n" +
+                "Số lượng Type: " + typeCount.ToString() + "\n" +
+                "Số lượng Instance: " + instanceCount.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Nguyen_Truong_Giang/Project-Revit/ExRevitAPI/ExRevitAPI/InfoParameter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace ExRevitAPI
+{
+    [Transaction(TransactionMode.Manual)]
+    class InfoParameter : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+
+            Reference r = uidoc.Selection.PickObject(ObjectType.Element);
+            Element element = uidoc.Document.GetElement(r);
+
+
+            ParameterSet paraset = element.Parameters;
+
+            foreach (Parameter parameter in paraset)
+            {
+                string parameterName = parameter.Definition.Name;
+                string parameterValue = parameter.AsValueString();
+                string paraDataType = parameter.Definition.ParameterType.ToString();
+
+                // In ra thông tin parameter
+                TaskDialog.Show("Parameter info", 
+                    "Parameter Name: " + parameterName + "\n"
+                    + "Parameter Value: " + parameterValue + "\n"
+                    + "Data Type: " + paraDataType);
+            }
+
+
+            return Result.Succeeded;
+        }
+    }
+}
