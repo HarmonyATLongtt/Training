c2a5853e3e7cf19f01d849909c32f3f1f883946d
Chin Su <viet129@hotmail.com>
2023-09-11 17:13:42 +0700

    Create column use Revit API

---
+++ Revit_API/CreateColumnApi/SelectCommand.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace CreateColumnApi
+{
+    [Transaction(TransactionMode.ReadOnly)]
+    public class SelectCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            Selection sel = uiDoc.Selection;
+            try
+            {
+                Element elem = doc.GetElement(sel.PickObject(ObjectType.Element, "Pick an element..."));
+
+                if (elem != null)
+                {
+                    string ans = "";
+                    foreach (Parameter p in elem.Parameters)
+                    {
+                        ans += p.Definition.Name + ": " + p.AsValueString() + "\n";
+                    }
+                    TaskDialog.Show("Information", ans);
+                }
+            }
+            catch { }
+
+            return Result.Succeeded;
+        }
+    }
+}
