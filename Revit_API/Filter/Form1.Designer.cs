e54ea0d4c18269623fffc06db4ece118ce0de2fc
Chin Su <viet129@hotmail.com>
2023-09-11 17:18:35 +0700

    Create filter for current view have overrides graphics to filter area of floor

---
+++ Revit_API/Filter/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboRules = new System.Windows.Forms.ComboBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.btnCommit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Area Of Floor You Want To Filter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Area is:";
+            // 
+            // cboRules
+            // 
+            this.cboRules.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboRules.FormattingEnabled = true;
+            this.cboRules.Items.AddRange(new object[] {
+            "Equals",
+            "Greater than",
+            "Less than"});
+            this.cboRules.Location = new System.Drawing.Point(61, 38);
+            this.cboRules.Name = "cboRules";
+            this.cboRules.Size = new System.Drawing.Size(112, 21);
+            this.cboRules.TabIndex = 2;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(189, 38);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(81, 20);
+            this.txtArea.TabIndex = 3;
+            // 
+            // btnCommit
+            // 
+            this.btnCommit.Location = new System.Drawing.Point(118, 76);
+            this.btnCommit.Name = "btnCommit";
+            this.btnCommit.Size = new System.Drawing.Size(75, 23);
+            this.btnCommit.TabIndex = 4;
+            this.btnCommit.Text = "Commit";
+            this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 126);
+            this.Controls.Add(this.btnCommit);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.cboRules);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboRules;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Button btnCommit;
+    }
+}
---
+++ Revit_API/Filter/Main.cs
@@ -0,0 +1,118 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Filter
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Main : IExternalCommand
+    {
+        [System.Obsolete]
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            Application app = uiApp.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            // Declare filter name
+            string filterName = "My Filter";
+
+            // Check filter name is exists ?
+            var checkFilterNameExists = new FilteredElementCollector(doc)
+                .OfClass(typeof(FilterElement))
+                .WhereElementIsNotElementType()
+                .Cast<FilterElement>()
+                .FirstOrDefault(e => e.Name.Equals((filterName)));
+
+            // If filter name is exists -> add to current view
+            if (checkFilterNameExists != null)
+            {
+                using (var trans = new Transaction(doc, "Add filter to current view"))
+                {
+                    trans.Start();
+
+                    doc.ActiveView.AddFilter(checkFilterNameExists.Id);
+
+                    trans.Commit();
+                }
+                return Result.Succeeded;
+            }
+
+            // Get line pattern type 'hidden'
+            var linePatternId = new FilteredElementCollector(doc)
+                .OfClass(typeof(LinePatternElement))
+                .WhereElementIsNotElementType()
+                .Where((e => e.Name.Equals("Hidden")))
+                .First().Id;
+
+            // Get fill pattern type Aluminum
+            var fillPatternId = new FilteredElementCollector(doc)
+                .OfClass(typeof(FillPatternElement))
+                .WhereElementIsNotElementType()
+                .Where(e => e.Name.Equals("Aluminum"))
+                .First().Id;
+
+            // Create a list type element id to save categories to filter
+            List<ElementId> cats = new List<ElementId>();
+            cats.Add(new ElementId(BuiltInCategory.OST_Floors));
+
+            // Get parameter to filter on category
+            ElementId parameter = new ElementId(BuiltInParameter.HOST_AREA_COMPUTED);
+
+            // Create override graphic
+            OverrideGraphicSettings overrideGraphic = new OverrideGraphicSettings();
+            overrideGraphic.SetSurfaceForegroundPatternColor((new Color(255, 0, 0)));
+            overrideGraphic.SetSurfaceForegroundPatternId(fillPatternId);
+            overrideGraphic.SetProjectionLinePatternId(linePatternId);
+            overrideGraphic.SetProjectionLineColor(new Color(125, 25, 200));
+
+            using (var trans = new Transaction(doc, "Create my filter..."))
+            {
+                trans.Start();
+
+                // Create element parameter filter to filter element
+                ElementParameterFilter elementFilter;
+
+                // Create a form enter input from user
+                Form1 form = new Form1();
+                form.ShowDialog();
+
+                // Convert units user to units internal project
+                float area = (float)UnitUtils.ConvertToInternalUnits(form.area, DisplayUnitType.DUT_SQUARE_METERS);
+
+                switch (form.compareArea)
+                {
+                    case "Equals":
+                        elementFilter = new ElementParameterFilter(ParameterFilterRuleFactory.CreateEqualsRule(parameter, area, 0.001));
+                        break;
+                    case "Greater than":
+                        elementFilter = new ElementParameterFilter(ParameterFilterRuleFactory.CreateGreaterRule(parameter, area, 0.001));
+                        break;
+                    case "Less than":
+                        elementFilter = new ElementParameterFilter(ParameterFilterRuleFactory.CreateLessRule(parameter, area, 0.001));
+                        break;
+                    default:
+                        elementFilter = new ElementParameterFilter(ParameterFilterRuleFactory.CreateGreaterRule(parameter, 0, 0.001));
+                        break;
+                }
+
+                // Create parameter filter element
+                ParameterFilterElement filterElement = ParameterFilterElement.Create(doc, filterName, cats, elementFilter);
+                // Add filter to current view
+                doc.ActiveView.AddFilter(filterElement.Id);
+
+                // Set filter override
+                doc.ActiveView.SetFilterOverrides(filterElement.Id, overrideGraphic);
+
+                trans.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
