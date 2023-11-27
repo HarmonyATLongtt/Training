78e341d0de4e7962793670647e7e33e2455db62d
manhdung02 <vmd111202@gmail.com>
2023-11-27 13:06:45 +0700

    update

--- Vu_Manh_Dung/OOP/OOP/Data/TaxData.cs
+++ Vu_Manh_Dung/OOP/OOP/Data/TaxData.cs
@@ -12,13 +12,6 @@
                 else if (Income <= 20000000) return 0.15f;
                 else return 0.2f;
             }
-
-            //if (Age < 18) return 0;
-            //if (Income <= 9000000) return 0.05f;
-            //else if (Income <= 15000000) return 0.1f;
-            //else if (Income <= 20000000) return 0.15f;
-            //else return 0.2f;
-
         }
     }
 }
