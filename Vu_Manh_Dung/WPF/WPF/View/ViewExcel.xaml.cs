cc2ce0cb313ebe21892d43c940b9c4b676cf18c0
manhdung02 <vmd111202@gmail.com>
2024-01-09 08:39:55 +0700

    wpf

---
+++ Vu_Manh_Dung/WPF/WPF/View/ViewExcel.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using WPF.Model;
+
+namespace WPF.View
+{
+    /// <summary>
+    /// Interaction logic for ViewExcel.xaml
+    /// </summary>
+    public partial class ViewExcel : UserControl
+    {
+        private List<Student> students = new List<Student>();
+
+        public ViewExcel()
+        {
+            InitializeComponent();
+            LoadStudent();
+        }
+
+        private void LoadStudent()
+        {
+            students.Add(new Student() { Id = 1, Name = "ABC", Age = 20, Address = "HN" });
+            students.Add(new Student() { Id = 2, Name = "ABC", Age = 20, Address = "HN" });
+            students.Add(new Student() { Id = 3, Name = "ABC", Age = 20, Address = "HN" });
+            students.Add(new Student() { Id = 4, Name = "ABC", Age = 20, Address = "HN" });
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            dgExcel.Columns.Clear();
+            dgExcel.ItemsSource = students;
+            //dgExcel.MaxColumnWidth = dgExcel.Width / dgExcel.Columns.Count;
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = "";
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Excel | *.xlsx | Excel 2003 | *.xls";
+            if(dialog.ShowDialog() == true)
+            {
+                filePath = dialog.FileName;
+            }
+            if (string.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Invalid path");
+                return;
+            }
+            try
+            {
+                using(ExcelPackage p = new ExcelPackage())
+                { 
+                    p.Workbook.Properties.Author = "VMD";
+                    p.Workbook.Properties.Title = "FileExport";
+                    p.Workbook.Worksheets.Add(Name);
+                    ExcelWorksheet ws = p.Workbook.Worksheets[1];
+                }
+            }catch(Exception ew)
+            {
+                MessageBox.Show(ew.ToString());
+            }
+        }
+    }
+}
---
+++ Vu_Manh_Dung/WPF/WPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF.Model;
+
+namespace WPF.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Student> Students { get; set; }
+
+        public ICommand ShowWindowCommand { get; set; }
+
+        public MainViewModel() {
+            Students = 
+        }
+
+    }
+}
