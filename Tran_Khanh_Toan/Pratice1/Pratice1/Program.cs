3b2c7552107d2a3e5deaba4def6ab741c8f6bf21
Khoa-Nguyen1998 <78605859+Khoa-Nguyen1998@users.noreply.github.com>
2024-01-15 09:45:05 +0700

    clean code

--- Tran_Khanh_Toan/Pratice1/Pratice1/Program.cs
+++ Tran_Khanh_Toan/Pratice1/Pratice1/Program.cs
@@ -1,38 +1,34 @@
-﻿using System;
-using System.IO;
-
+﻿//xóa các thư viện không dùng đến
 namespace Pratice1
 {
     internal class Program
     {
-        public bool canWin(int[] game, int leap, int i)
+        public static bool canWin(int[] game, int leap, int i)
         {
             if (i < 0 || game[i] == 1) return false;
-            if (i + leap >= game.Length || i == game.Length - 1) return true;
+            if (i + leap >= game.Length - 1) return true; //điều kiện nên ngắn gọn dễ hiểu ,tránh làm rối logic
             game[i] = 1;
             return canWin(game, leap, i + leap) || canWin(game, leap, i + 1) || canWin(game, leap, i - 1);
         }
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
-            Program program = new Program();
             string[] lines = File.ReadAllLines("../input.txt");
             int q = int.Parse(lines[0]);
             int currentLine = 1;
             string output = "";
             for (int j = 0; j < q; j++)
             {
-                string[] arrLeap = lines[currentLine].Split(" ");
+                string[] arrLeap = lines[currentLine++].Split(" ");
                 int leap = int.Parse(arrLeap[1]);
                 int lg = int.Parse(arrLeap[0]);
-                currentLine++;
-                string[] gameStrings = lines[currentLine].Split(" ");
+                string[] gameStrings = lines[currentLine++].Split(" ");
                 int[] game = Array.ConvertAll(gameStrings, int.Parse);
-                currentLine++;
 
-                bool result = program.canWin(game, leap, 0);
+                bool result = canWin(game, 0, 0);
                 output += result ? "YES\n" : "NO\n";
             }
-                File.WriteAllLines("../output.txt", new string[] { output });
+            File.WriteAllLines("../output.txt", new string[] { output });
         }
     }
 }
