70f7b6239c5f2ac3893ecec1bfc464cd808febc6
HungNB <kugura789@gmail.com>
2024-05-23 16:10:24 +0700

    update control

---
+++ Nguyen-Ba-Hung/WPF/WPF_Example/WPF_Example/ViewModels/PersonViewModel.cs
@@ -0,0 +1,29 @@
+﻿using WPF_Example.Models;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WPF_Example.ViewModels
+{
+    internal class PersonViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public ObservableCollection<Person> People { get; set; }
+
+        public PersonViewModel()
+        {
+            People = new ObservableCollection<Person>();
+        }
+    }
+}
