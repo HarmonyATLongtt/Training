c721b36ab53f29eefb5f2572f2ff1887c385c3c1
DuyAnh <duyanhdao297@gmail.com>
2024-08-08 13:35:06 +0700

    Sua bai tap 3

--- Dao-Duy-Anh/Project-OOP/Baitap-3/Program.cs
+++ Dao-Duy-Anh/Project-OOP/Baitap-3/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Baitap_3
@@ -12,7 +17,7 @@ namespace Baitap_3
         string GetInfo();
     }
 
-    class Person : IPerson
+    public class Person : IPerson
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -29,23 +34,23 @@ namespace Baitap_3
             TaxCoe = taxData.GetTaxCoe(age, income);
         }
 
-        public bool Equals(Person p)
+        public virtual bool Equals(Person p)
         {
             if (p == null) return false;
             return Id == p.Id && Name == p.Name && Age == p.Age && TaxCoe == p.TaxCoe && Income == p.Income && TaxCoe == p.TaxCoe;
         }
 
-        public double GetTax()
+        public virtual double GetTax()
         {
             return Income * TaxCoe;
         }
 
-        public string GetInfo()
+        public virtual string GetInfo()
         {
             return $"Id: {Id}, Name: {Name}, Age: {Age}, Income: {Income}, TaxCoe: {TaxCoe}";
         }
     }
-    class TaxData
+    public class TaxData
     {
         public double GetTaxCoe(int age, double income)
         {
@@ -76,27 +81,30 @@ namespace Baitap_3
             return heSoThue;
         }
     }
-    class Student : IPerson
+    public class Student : Person
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public double Income { get; set; }
-        public double TaxCoe { get; set; }
+        //public new int Id { get; set; }
+        //public new string Name { get; set; }
+        //public new int Age { get; set; }
+        //public new double Income { get; set; }
+        //public new double TaxCoe { get; set; }
         public string School { get; set; }
         public string Class { get; set; }
 
 
-        public Student(int id, string name, int age, string school, string className)
+        public Student(int id, string name, int age, double income, TaxData taxData, string school, string className) 
+            : base ( id,  name,  age,  income, taxData)
         {
             Id = id;
             Name = name;
             Age = age;
+            Income = income;
+            TaxCoe = taxData.GetTaxCoe(age, income);
             School = school;
             Class = className;
         }
 
-        public double GetTax()
+        public override double GetTax()
         {
             return Income * TaxCoe;
         }
@@ -110,21 +118,22 @@ namespace Baitap_3
             return Id == s.Id && Name == s.Name && Age == s.Age;
         }
 
-        public string GetInfo()
+        public override string GetInfo()
         {
             return $"Student - ID: {Id} \n - Name: {Name} \n - Age: {Age} \n - School: {School} \n - Class: {Class} \n";
         }
     }
-    class Teacher : IPerson
+    public class Teacher : Person
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public double Income { get; set; }
-        public double TaxCoe { get; set; }
+        //public int Id { get; set; }
+        //public string Name { get; set; }
+        //public int Age { get; set; }
+        //public double Income { get; set; }
+        //public double TaxCoe { get; set; }
         public string School { get; set; }
 
         public Teacher(int id, string name, int age, double income, TaxData taxData, string school)
+            : base(id, name, age, income, taxData)
         {
             Id = id;
             Name = name;
@@ -134,7 +143,7 @@ namespace Baitap_3
             School = school;
         }
 
-        public double GetTax()
+        public override double GetTax()
         {
             return Income * TaxCoe;
         }
@@ -148,22 +157,23 @@ namespace Baitap_3
             return Id == teacher.Id && Name == teacher.Name && Age == teacher.Age && Income == teacher.Income;
         }
 
-        public string GetInfo()
+        public override string GetInfo()
         {
             return $"Teacher - ID: {Id} \n - Name: {Name} \n - Age: {Age} \n - School: {School} \n - Income: {Income} \n - Tax: {GetTax()} \n";
         }
     }
-    class Employee : IPerson
+    public class Employee : Person
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public double Income { get; set; }
-        public double TaxCoe { get; set; }
+        //public int Id { get; set; }
+        //public string Name { get; set; }
+        //public int Age { get; set; }
+        //public double Income { get; set; }
+        //public double TaxCoe { get; set; }
         public string Company { get; set; }
         public string JobTitle { get; set; }
 
         public Employee(int id, string name, int age, string company, string jobtitle, double income, TaxData taxData)
+            : base(id, name, age, income, taxData)
         {
             Id = id;
             Name = name;
@@ -174,86 +184,70 @@ namespace Baitap_3
             TaxCoe = taxData.GetTaxCoe(age, income);
         }
 
-        public double GetTax()
+        public override double GetTax()
         {
             return Income * TaxCoe;
         }
 
-        public bool Equals(Person Teacher)
+        public bool Equals(Teacher teacher)
         {
-            if (Teacher == null)
+            if (teacher == null)
             {
                 return false;
             }
-            return Id == Teacher.Id && Name == Teacher.Name && Age == Teacher.Age && Income == Teacher.Income;
+            return Id == teacher.Id && Name == teacher.Name && Age == teacher.Age && Income == teacher.Income;
         }
 
-        public string GetInfo()
+        public override string GetInfo()
         {
             return $"Employee - ID: {Id} \n - Name: {Name} \n - Age: {Age} \n - Company: {Company} \n - JobTitle: {JobTitle} \n - Income: {Income} \n - Tax: {GetTax()} \n";
         }
     }
     internal class Program
     {
-        
+        static TaxData taxData = new TaxData();
+        static List<Person> listPeople = new List<Person>();
+        static void Output()
+        {
+            Console.WriteLine("Total Student:" +listPeople.Count(p=>p is Student));
+            foreach (Person person in listPeople.Where(p=>p is Student))
+            {
+                Console.WriteLine(person.GetInfo());
+            }
+            Console.WriteLine("Total Teacher:" + listPeople.Count(p => p is Teacher));
+            foreach (Person person in listPeople.Where(p => p is Teacher))
+            {
+                Console.WriteLine(person.GetInfo());
+            }
+            Console.WriteLine("Total Employee:" + listPeople.Count(p => p is Employee));
+            foreach (Person person in listPeople.Where(p => p is Employee))
+            {
+                Console.WriteLine(person.GetInfo());
+            }
+        }
+
+        static void Init()
+        {
+            //List<Student> studentList = new List<Student>();
+            listPeople.Add(new Student(1, "My", 17, 0, taxData, "School A", "Class 1"));
+            listPeople.Add(new Student(2, "Linh", 18, 0, taxData, "School B", "Class 2"));
+            listPeople.Add(new Student(2, "Duy", 18, 0, taxData, "School B", "Class 2"));
+
+            //List<Teacher> teacherList = new List<Teacher>();
+            listPeople.Add(new Teacher(1, "Quang", 27, 15000000, taxData, "School A"));
+            listPeople.Add(new Teacher(2, "Anh", 29, 25000000, taxData, "School B"));
+
+
+            //List<Employee> employeeList = new List<Employee>();
+            listPeople.Add(new Employee(1, "Dat", 27, "Harmony A", "Deverloper", 30000000, taxData));
+            listPeople.Add(new Employee(2, "Giang", 29, "Harmony B", "Tester", 25000000, taxData));
+        }
+
         static void Main(string[] args)
         {
-            TaxData taxData = new TaxData();
-
-            List<Student> studentList = new List<Student>();
-            studentList.Add(new Student(1, "My", 17, "School A", "Class 1"));
-            studentList.Add(new Student(2, "Linh", 18, "School B", "Class 2"));
-
-            List<Teacher> teacherList = new List<Teacher>();
-            teacherList.Add(new Teacher(1, "Quang", 27, 15000000, taxData, "School A"));
-            teacherList.Add(new Teacher(2, "Anh", 29, 25000000, taxData, "School B"));
-
-
-            List<Employee> employeeList = new List<Employee>();
-            employeeList.Add(new Employee(1, "Jinx", 27, "Harmony A", "Deverloper", 30000000, taxData));
-            employeeList.Add(new Employee(2, "Giang", 29, "Harmony B", "Tester", 25000000, taxData));
-
-            List<Person> people = new List<Person>();
-            OutputStudent(studentList);
-            Console.WriteLine("***************************** \n");
-            OutputTeacher(teacherList);
-            Console.WriteLine("***************************** \n");
-            OutputEmployee(employeeList);
-
-
-            
-        }
-        public static void OutputStudent(List<Student> studentList)
-        {
-
-            foreach (var students in studentList)
-            {
-                Console.WriteLine($"{students.GetInfo()}");
-            }
-            Console.WriteLine($"Total Students: {studentList.Count}\n");
-            Console.ReadLine();
-        }
-
-        public static void OutputTeacher(List<Teacher> teacherList)
-        {
-
-            foreach (var teachers in teacherList)
-            {
-                Console.WriteLine($"{teachers.GetInfo()}");
-            }
-            Console.WriteLine($"Total Teacher: {teacherList.Count}\n");
-            Console.ReadLine();
-        }
-
-        public static void OutputEmployee(List<Employee> employeeList)
-        {
-
-            foreach (var employees in employeeList)
-            {
-                Console.WriteLine($"{employees.GetInfo()}");
-            }
-            Console.WriteLine($"Total Employees: {employeeList.Count}\n");
-            Console.ReadLine();
+            Init();
+            Output();
+            Console.ReadKey();
         }
     }
 }
