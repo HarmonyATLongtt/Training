3ac962a587a815262b01220a9d9ee8df558c77aa
linh vũ <vngclinh.work@gmail.com>
2024-09-10 16:07:33 +0700

    syntax

--- Do-Thai-Son/WpfEx/WpfEx/obj/Debug/net5.0-windows/WpfEx.AssemblyInfo.cs
+++ Do-Thai-Son/WpfEx/WpfEx/obj/Debug/net5.0-windows/WpfEx.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfEx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f64a34948751d4e544403253cf900ade683495")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfEx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfEx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
