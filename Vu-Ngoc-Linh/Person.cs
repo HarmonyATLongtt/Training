2d627f20f6914e2bc34b06966f8268268984a7ed
linh vũ <vngclinh.work@gmail.com>
2024-09-18 14:02:16 +0700

    Commit bai 1

---
+++ Vu-Ngoc-Linh/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_OOP
+{
+    public class Person
+    {
+        private string Id;
+        private string name;
+        private int age;
+        private double income;
+        private double taxCoe;
+
+        public string ID
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int Age
+        {
+            get { return Age; }
+            set { age = value; }
+        }
+        public double Income
+        {
+            set { income = value; }
+            get{ return income;}
+        }
+        public double TaxCoe
+        {
+            set{ taxCoe = value; }
+            get { return taxCoe; }
+        }
+        public bool Equals(Person a)
+        {
+            return ID == a.ID && Name == a.Name && Age == a.Age && Income == a.Income && TaxCoe == a.TaxCoe;
+        }
+        public double GetTax()
+        {
+            return Income * TaxCoe;
+        }
+    }
+}
---
+++ Vu-Ngoc-Linh/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_OOP
+{
+    internal class Program
+    {
+        static List<Person> peopleList = new List<Person>();
+        static void Init(string id, string name, int age, double income, double taxcoe)
+        {
+            peopleList.Add(new Person { ID = id, Name = name, Age = age, Income = income, TaxCoe = taxcoe });
+        }
+        static void Output()
+        {
+            foreach(Person person in peopleList)
+            {
+                Console.WriteLine($"ID:{person.ID}, Name:{person.Name}, Tax:{person.GetTax()}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string id = Console.ReadLine();
+                if (string.IsNullOrEmpty(id)) break;
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+                double income = double.Parse(Console.ReadLine());
+                double taxcoe = double.Parse(Console.ReadLine());
+                Init(id, name, age, income, taxcoe);
+            }
+            Output();
+            Console.ReadKey();
+        }
+    }
+}
