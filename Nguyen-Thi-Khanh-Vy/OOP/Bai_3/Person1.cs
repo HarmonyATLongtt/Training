31ea3e88d0c4e5514eee67b5db8a2114bf9270dc
nguyenvy49 <minvy9349@gmail.com>
2024-09-23 14:31:51 +0700

    sua 2

--- Nguyen-Thi-Khanh-Vy/OOP/Bai_3/Person1.cs
+++ Nguyen-Thi-Khanh-Vy/OOP/Bai_3/Person1.cs
@@ -3,56 +3,59 @@ using System.Security.Claims;
 using Bai_3;
 using OOP.Bai_3;
 
-public class Person1 : IPerson
+namespace Bai_3
 {
-	protected string ID;
-	protected string Name;
-	protected int Age;
-	
-	public Person1()
-	{
-	}
-	public Person1 (string id, string name, int age)
-	{
-		ID = id;
-		Name = name;
-		Age = age;
-		
-	}
-    protected static string GetInput(string prompt)
+    public class Person1 : IPerson
     {
-        string input;
-        do
-        {
-            Console.WriteLine(prompt);
-            input = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                Console.WriteLine("Gia tri nhap khong duoc de trong.Vui long nhap lai!");
-            }
-        } while (string.IsNullOrWhiteSpace(input));
-        return input;
-    }
+        protected string _id;
+        protected string _name;
+        protected int _age;
 
-    public void Nhap()
-    {
-        ID = GetInput("Nhap ID:");
-        Name = GetInput("Nhap Ho ten: ");
-
-        while (true)
+        public Person1()
         {
-            string ageInput = GetInput("Nhap tuoi: ");
-            if (int.TryParse(ageInput, out int age) && age > 0)
-            {
-                Age = age;
-                break;
-            }
-            Console.WriteLine("Tuoi khong hop le vui lpng nhap lai");
         }
-       
-    }
-    public string GetInfo()
-    {
-        return $"ID: {ID}, Name: {Name}, Age: {Age} ";
+        public Person1(string id, string name, int age)
+        {
+            _id = id;
+            _name = name;
+            _age = age;
+
+        }
+        protected static string GetInput(string prompt)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine(prompt);
+                input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Gia tri nhap khong duoc de trong.Vui long nhap lai!");
+                }
+            } while (string.IsNullOrWhiteSpace(input));
+            return input;
+        }
+
+        public void Nhap()
+        {
+            _id = GetInput("Nhap ID:");
+            _name = GetInput("Nhap Ho ten: ");
+
+            while (true)
+            {
+                string ageInput = GetInput("Nhap tuoi: ");
+                if (int.TryParse(ageInput, out int age) && age > 0)
+                {
+                    _age = age;
+                    break;
+                }
+                Console.WriteLine("Tuoi khong hop le vui lpng nhap lai");
+            }
+
+        }
+        public string GetInfo()
+        {
+            return $"ID: {_id}, Name: {_name}, Age: {_age} ";
+        }
     }
 }
