e5d2876c7c9a761b7ce1717befce8c2a9bf03d93
longtt <38779673+longt61@users.noreply.github.com>
2024-10-17 11:14:09 +0700

    update review

--- Vu-Ngoc-Linh/Bai1_WPF/ViewModel/MainViewModel.cs
+++ Vu-Ngoc-Linh/Bai1_WPF/ViewModel/MainViewModel.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -22,10 +21,23 @@ namespace Bai1_WPF.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        public MainModel Model { get; private set; }
+
+        public string FilePath_2
+        {
+            get => Model.FilePath;
+            set
+            {
+                Model.FilePath = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ICommand ImportData { get; set; }
         public ICommand ExportData { get; set; }
 
         private ObservableCollection<string> _sheetNames;
+
         public ObservableCollection<string> SheetNames
         {
             get { return _sheetNames; }
@@ -35,7 +47,9 @@ namespace Bai1_WPF.ViewModel
                 OnPropertyChanged(nameof(SheetNames));
             }
         }
+
         private string _selectedSheet;
+
         public string SelectedSheet
         {
             get { return _selectedSheet; }
@@ -50,29 +64,34 @@ namespace Bai1_WPF.ViewModel
                 }
             }
         }
+
+        public ObservableCollection<DataTable> Tables { get; set; }
+        public DataTable SelectedTable { get; set; }
+
         public Dictionary<string, DataTable> tables = new Dictionary<string, DataTable>();
+
         private void Upload(string selectedSheet)
         {
-            foreach(string sheetname in tables.Keys)
+            foreach (string sheetname in tables.Keys)
             {
-                if(sheetname == selectedSheet)
+                if (sheetname == selectedSheet)
                 {
                     Data = tables.GetValueOrDefault(sheetname);
                 }
             }
         }
 
-
         public MainViewModel()
         {
             _sheetNames = new ObservableCollection<string>();
             ImportData = new RelayCommand(ImportFile, CanImportFile);
             ExportData = new RelayCommand(ExportFile, CanExportFile);
-
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
         private DataTable _data;
+
         public DataTable Data
         {
             get { return _data; }
@@ -82,7 +101,9 @@ namespace Bai1_WPF.ViewModel
                 OnPropertyChanged();
             }
         }
+
         private string _filePath;
+
         public string FilePath
         {
             get { return _filePath; }
@@ -97,7 +118,9 @@ namespace Bai1_WPF.ViewModel
         {
             return true;
         }
-        public DataTable Student, Teacher, Employee;
+
+        //public DataTable Student, Teacher, Employee;
+
         private void ImportFile(object obj)
         {
             string filePath = "";
@@ -120,20 +143,18 @@ namespace Bai1_WPF.ViewModel
             {
                 ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
                 SheetNames.Clear();
-                foreach ( ExcelWorksheet worksheet in package.Workbook.Worksheets)
+                foreach (ExcelWorksheet worksheet in package.Workbook.Worksheets)
                 {
                     SheetNames.Add(worksheet.Name);
                     dt = worksheet.Cells[worksheet.Dimension.Start.Row, worksheet.Dimension.Start.Column,
                                     worksheet.Dimension.End.Row, worksheet.Dimension.End.Column].ToDataTable();
                     tables.Add(worksheet.Name, dt);
                 }
-                if (SheetNames.Any())
-                {
-                    SelectedSheet = SheetNames.First();
-                }
+                SelectedSheet = SheetNames.FirstOrDefault();
 
             }
         }
+
         private void ExportFile(object obj)
         {
             SaveFileDialog saveDialog = new SaveFileDialog();
@@ -169,9 +190,7 @@ namespace Bai1_WPF.ViewModel
                     MessageBox.Show("Export successfully.");
                     return;
                 }
-
             }
-
         }
 
         private bool CanExportFile(object obj)
@@ -179,10 +198,10 @@ namespace Bai1_WPF.ViewModel
             return true;
             //return SheetNames.Any();
         }
+
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
     }
 }
