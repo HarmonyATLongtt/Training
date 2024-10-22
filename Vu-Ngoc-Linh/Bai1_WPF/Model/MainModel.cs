9f281a39e4ae16c6fbc4ca6aa184fa1c115cf9e0
linh vũ <vngclinh.work@gmail.com>
2024-10-22 17:05:28 +0700

    update

--- Vu-Ngoc-Linh/Bai1_WPF/Model/MainModel.cs
+++ Vu-Ngoc-Linh/Bai1_WPF/Model/MainModel.cs
@@ -9,10 +9,6 @@ namespace Bai1_WPF.Model
     public class MainModel
     {
         public string FilePath { get; set; }
-<<<<<<< HEAD
         public string SheetName {  get; set; }
-=======
-        public string SheetName { get; set; }
->>>>>>> e5d2876c7c9a761b7ce1717befce8c2a9bf03d93
     }
 }
