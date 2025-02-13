cb51b2f0435d4c7f16b3b417afed1d83e8bb0ae7
nguyenvy49 <minvy9349@gmail.com>
2025-02-13 10:37:08 +0700

    Update 13/2

---
+++ Nguyen-Thi-Khanh-Vy/WPF_Ex/WPF_Ex/Behaviors/WindowBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace WPF_Ex.Behaviors
+{
+    public class WindowBehavior : Behavior<Window>
+    {
+        // Command được bind cho phím Escape
+        public ICommand EscapeCommand
+        {
+            get { return (ICommand)GetValue(EscapeCommandProperty); }
+            set { SetValue(EscapeCommandProperty, value); }
+        }
+        public static readonly DependencyProperty EscapeCommandProperty =
+            DependencyProperty.Register("EscapeCommand", typeof(ICommand), typeof(WindowBehavior), new PropertyMetadata(null));
+
+        // Command được bind cho phím F5
+        public ICommand F5Command
+        {
+            get { return (ICommand)GetValue(F5CommandProperty); }
+            set { SetValue(F5CommandProperty, value); }
+        }
+        public static readonly DependencyProperty F5CommandProperty =
+            DependencyProperty.Register("F5Command", typeof(ICommand), typeof(WindowBehavior), new PropertyMetadata(null));
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewKeyDown += OnPreviewKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewKeyDown -= OnPreviewKeyDown;
+            base.OnDetaching();
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                // Truyền cửa sổ (AssociatedObject) làm parameter
+                if (EscapeCommand != null && EscapeCommand.CanExecute(AssociatedObject))
+                {
+                    EscapeCommand.Execute(AssociatedObject);
+                }
+                else
+                {
+                    AssociatedObject.Close();
+                }
+                e.Handled = true;
+            }
+            else if (e.Key == Key.F5)
+            {
+                if (F5Command != null && F5Command.CanExecute(AssociatedObject))
+                {
+                    F5Command.Execute(AssociatedObject);
+                    e.Handled = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ Nguyen-Thi-Khanh-Vy/WPF_Ex/WPF_Ex/ViewModels/EventToCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace WPF_Ex.Behaviors
+{
+    /// <summary>
+    /// Behavior này chuyển đổi một sự kiện thành một command.
+    /// Nếu PassEventArgsToCommand được đặt là true, thì đối tượng event (EventArgs) sẽ được truyền làm CommandParameter,
+    /// trừ khi CommandParameter đã được set.
+    /// </summary>
+    public class EventToCommand : TriggerAction<DependencyObject>
+    {
+        #region Dependency Properties
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(EventToCommand), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Command sẽ được gọi khi sự kiện được kích hoạt.
+        /// </summary>
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(object), typeof(EventToCommand), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Tham số sẽ được truyền cho Command khi nó được thực thi.
+        /// Nếu để trống và PassEventArgsToCommand = true thì đối tượng event (EventArgs) sẽ được truyền vào.
+        /// </summary>
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty PassEventArgsToCommandProperty =
+            DependencyProperty.Register("PassEventArgsToCommand", typeof(bool), typeof(EventToCommand), new PropertyMetadata(false));
+
+        /// <summary>
+        /// Nếu true, đối tượng sự kiện (EventArgs) sẽ được truyền cho Command nếu CommandParameter chưa được set.
+        /// </summary>
+        public bool PassEventArgsToCommand
+        {
+            get { return (bool)GetValue(PassEventArgsToCommandProperty); }
+            set { SetValue(PassEventArgsToCommandProperty, value); }
+        }
+
+        #endregion
+
+        protected override void Invoke(object parameter)
+        {
+            if (Command == null)
+            {
+                return;
+            }
+
+            object commandParameter = CommandParameter;
+
+            // Nếu CommandParameter chưa được set và PassEventArgsToCommand = true, sử dụng đối tượng event (parameter)
+            if (commandParameter == null && PassEventArgsToCommand)
+            {
+                commandParameter = parameter;
+            }
+
+            if (Command.CanExecute(commandParameter))
+            {
+                Command.Execute(commandParameter);
+            }
+        }
+    }
+}
