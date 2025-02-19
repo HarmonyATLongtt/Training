44c10dc130b76039111a26398de60695043898c6
Ba Duc <ducnbcntt1.01k57@gmail.com>
2025-02-19 13:08:48 +0700

    nop bai tap OOP

---
+++ Nguyen-Ba-Duc/OOP/OOP/Bai_2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class TaxData
+    {
+        public float GetTaxCoe(int age, float income)
+        {
+            float Taxcoe = 0.0f;
+            if (age < 18)
+            {
+                Taxcoe = 0.0f;
+                return Taxcoe;
+            }
+            if (income <= 9000000)
+            {
+                Taxcoe = 0.05f;
+            }
+            else if (income > 9000000 && income <= 15000000)
+            {
+                Taxcoe = 0.1f;
+            }
+            else if (income > 15000000 && income <= 20000000)
+            {
+                Taxcoe = 0.15f;
+            }
+            else if (income > 20000000 && income <= 30000000)
+            {
+                Taxcoe = 0.2f;
+            }
+            else
+            {
+                Taxcoe = 0.3f;
+            }
+
+            return Taxcoe;
+        }
+    }
+}
